e0ff44423ad5e5bb730d45c2a10323399c364615
shahbaz036 <sha42ali@gmail.com>
2025-03-23 04:01:09 +0530

    webscraper

--- amazon-scraper-frontend/amazon-scraper-backend/render-build.sh
+++ amazon-scraper-frontend/amazon-scraper-backend/render-build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 npm install
 npx puppeteer browsers install chrome
+
+# Check if Chrome is installed
+echo "Checking if Chromium is installed..."
+ls -l /opt/render/.cache/puppeteer/chrome
